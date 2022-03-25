d3de94ad3642469ee3691f25872fc954ff6d1c08
xianzhen16 <66318963+xianzhen16@users.noreply.github.com>
2022-03-24 23:14:41 -0400

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -31,13 +31,15 @@ could be replaced with PostgreSQL or Pandas or straight python lists
 
 '''
 
-#from transactions import Transaction
+# from transactions import Transaction
+from datetime import datetime
+from enum import Enum
+
 from category import Category
 import sys
 
-#transactions = Transaction('tracker.db')
+# transactions = Transaction('tracker.db')
 category = Category('tracker.db')
-
 
 # here is the menu for the tracker app
 
@@ -58,35 +60,34 @@ menu = '''
 
 
 def process_choice(choice):
-
     # 0. quit
-    if choice=='0': 
+    if choice == '0':
         return
     # 1. show categories
-    elif choice=='1':
+    elif choice == '1':
         cats = category.select_all()
         print_categories(cats)
     # 2. add category
-    elif choice=='2':
+    elif choice == '2':
         name = input("category name: ")
         desc = input("category description: ")
-        cat = {'name':name, 'desc':desc}
+        cat = {'name': name, 'desc': desc}
         category.add(cat)
     # 3. modify category
-    elif choice=='3':
+    elif choice == '3':
         print("modifying category")
         rowid = int(input("rowid: "))
         name = input("new category name: ")
         desc = input("new category description: ")
-        cat = {'name':name, 'desc':desc}
-        category.update(rowid,cat)
+        cat = {'name': name, 'desc': desc}
+        category.update(rowid, cat)
     # 4. show transactions
-    elif choice=='4':
+    elif choice == '4':
         for cat in category:
             print(cat)
     # 5. add transaction
     # Author: Kelly Zhang
-    elif choice=='5':
+    elif choice == '5':
         print("adding a transaction")
         name = input("new category name: ")
         desc = input("new category description: ")
@@ -94,36 +95,52 @@ def process_choice(choice):
         category.add(cat)
     # 6. delete transaction
     # Author: Kelly Zhang
-    elif choice=='6': 
+    elif choice == '6':
         print("deleting a transaction")
         rowid = int(input("rowid to be deleted: "))
         category.delete(rowid)
     # 7. summarize transactions by date
-    elif choice=='7':
+    # Author: Xianzhen Zhao
+    elif choice == '7':
         print("summarizing transactions by date")
+        try:
+            date_input = input("Please input date(YYYY-MM-DD): ")
+            validate(date_input, DateFormat.DATE)
+            cats = category.select_by_date(date_input)
+            print_categories(cats)
+        except ValueError as error:
+            print(str(error))
 
     # 8. summarize transactions by month
-    elif choice=='8':
+    # Author: Xianzhen Zhao
+    elif choice == '8':
         print("summarizing transactions by month")
+        try:
+            date_input = input("Please input date(YYYY-MM): ")
+            validate(date_input, DateFormat.MONTH)
+            cats = category.select_by_month(date_input)
+            print_categories(cats)
+        except ValueError as error:
+            print(str(error))
 
     # 9. summarize transactions by year
-    elif choice=='9': 
+    elif choice == '9':
         print("summarizing transactions by year")
 
     # 10. summarize transactions by category
-    elif choice=='10':
+    elif choice == '10':
         print("summarizing transactions by category")
 
     # 11. print this menu
     # Author: Kelly Zhang
-    elif choice=='11':
+    elif choice == '11':
         print("menu: ", menu)
     # all other choices
     else:
-        print("choice",choice,"not yet implemented")
+        print("choice", choice, "not yet implemented")
 
     choice = input("> ")
-    return(choice)
+    return (choice)
 
 
 def toplevel():
@@ -132,9 +149,10 @@ def toplevel():
     ''' read the command args and process them'''
     print(menu)
     choice = input("> ")
-    while choice !='0' :
+    while choice != '0':
         choice = process_choice(choice)
     print('bye')
+
 
 #
 # here are some helper functions
@@ -142,28 +160,55 @@ def toplevel():
 
 def print_transactions(items):
     ''' print the transactions '''
-    if len(items)==0:
+    if len(items) == 0:
         print('no items to print')
         return
     print('\n')
-    print("%-10s %-10d %-10s %-10d %-30s"%(
-        'item #','amount','category','date','description'))
-    print('-'*40)
+    print("%-10s %-10d %-10s %-10d %-30s" % (
+        'item #', 'amount', 'category', 'date', 'description'))
+    print('-' * 40)
     for item in items:
-        values = tuple(item.values()) 
-        print("%-10s %-10d %-10s %-10d %-30s"%values)
+        values = tuple(item.values())
+        print("%-10s %-10d %-10s %-10d %-30s" % values)
+
 
 def print_category(cat):
-    print("%-3d %-10s %-30s"%(cat['rowid'],cat['name'],cat['desc']))
+    print("%-3d %-10s %-30s" % (cat['rowid'], cat['name'], cat['desc']))
+
 
 def print_categories(cats):
-    print("%-3s %-10s %-30s"%("id","name","description"))
-    print('-'*45)
+    print("%-3s %-10s %-30s" % ("id", "name", "description"))
+    print('-' * 45)
     for cat in cats:
         print_category(cat)
+
+
+def validate(date_text, date_format):
+    try:
+        if date_format == DateFormat.DATE:
+            format_str = "%Y-%m-%d"
+            if date_text != datetime.strptime(date_text, format_str).strftime(format_str):
+                raise ValueError("Wrong Date Input Format, yyyy-mm-dd")
+            return True
+        elif date_format == DateFormat.MONTH:
+            format_str = "%Y-%m"
+            if date_text != datetime.strptime(date_text, format_str).strftime(format_str):
+                raise ValueError("Wrong Date Input Format, yyyy-mm")
+            return True
+        elif date_format == DateFormat.YEAR:
+            if date_text != datetime.strptime(date_text, "%Y").strftime('%Y'):
+                raise ValueError("Wrong Date Input Format, yyyy")
+            return True
+    except ValueError:
+        raise ValueError("Wrong Date Input Format")
+
+
+class DateFormat(Enum):
+    DATE = 1
+    MONTH = 2
+    YEAR = 3
 
 
 # here is the main call!
 
 toplevel()
-
